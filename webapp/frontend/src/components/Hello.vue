e679794bba161954982c886fa9f37d1d04bc44c9
NeverMoes <nevermoes@gmail.com>
2017-02-13 00:14:43 +0800

    init vue

---
+++ webapp/frontend/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1> {{ greeting }} </h1>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
