47018019474f53e450100d45d638d6e0e68a58b8
NeverMoes <nevermoes@gmail.com>
2017-02-16 17:30:34 +0800

    init frontend

--- webapp/frontend/src/main.js
+++ webapp/frontend/src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
