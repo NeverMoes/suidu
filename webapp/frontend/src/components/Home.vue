bb09816b6d2c2b2acc93d05c1fd8f5222df133f7
NeverMoes <nevermoes@gmail.com>
2017-02-17 23:09:40 +0800

    init frontend

---
+++ webapp/frontend/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- webapp/frontend/src/main.js
+++ webapp/frontend/src/main.js
@@ -1,8 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+import Vueresource from 'vue-resource'
 import router from './router'
+import App from './App'
+
+Vue.config.debug = true;
+
+Vue.use(ElementUI);
+vue.use(Vueresource);
 
 /* eslint-disable no-new */
 new Vue({
