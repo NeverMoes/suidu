01423855f1267d2474ddadabe5d5a00abf8c2aa5
NeverMoes <nevermoes@gmail.com>
2017-08-19 22:23:38 +0800

    add build

---
+++ deploy/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker build -t suidu:django ./django/
+docker build -t suidu:mongodb ./mongodb/
+docker build -t suidu:nginx ./nginx/
+docker build -t suidu:scrapy ./scrapy/
+docker build -t suidu:scrapyd ./scrapyd/
+docker build -t suidu:vue ./vue/
